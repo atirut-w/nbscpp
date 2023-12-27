59197dcb0f53a35ad4dfe3ff8be42ead3ba1ea0e
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-12-27 18:49:41 +0700

    WIP header loading

--- include/nbs.hpp
+++ include/nbs.hpp
@@ -1,1 +1,19 @@
 #pragma once
+#include <istream>
+#include <string>
+
+namespace NBS
+{
+    class SongData
+    {
+    public:
+        SongData(std::istream &stream);
+
+        int length;
+        int height;
+        std::string name;
+        std::string author;
+        std::string original_author;
+        std::string description;
+    };
+}
--- src/nbs.cpp
+++ src/nbs.cpp
@@ -1,1 +1,43 @@
 #include <nbs.hpp>
+#include <cstdint>
+#include <string>
+
+int8_t read_byte(std::istream &stream)
+{
+    int8_t value;
+    stream.read(reinterpret_cast<char *>(&value), sizeof(value));
+    return value;
+};
+
+int16_t read_short(std::istream &stream)
+{
+    int16_t value;
+    stream.read(reinterpret_cast<char *>(&value), sizeof(value));
+    return value;
+};
+
+int32_t read_int(std::istream &stream)
+{
+    int32_t value;
+    stream.read(reinterpret_cast<char *>(&value), sizeof(value));
+    return value;
+};
+
+std::string read_pstring(std::istream &stream)
+{
+    int8_t length = read_int(stream);
+    std::string value;
+    value.resize(length);
+    stream.read(value.data(), length);
+    return value;
+};
+
+NBS::SongData::SongData(std::istream &stream)
+{
+    length = read_short(stream);
+    height = read_short(stream);
+    name = read_pstring(stream);
+    author = read_pstring(stream);
+    original_author = read_pstring(stream);
+    description = read_pstring(stream);
+};
